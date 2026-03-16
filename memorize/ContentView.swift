3f2ab5b1fe632ada8846a2ff8e7f04990f76e47f
yanistall <334488556a@gmail.com>
2026-03-16 17:50:06 +0800

    README.md

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "cat")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("早上好")
         }
         .padding()
     }
